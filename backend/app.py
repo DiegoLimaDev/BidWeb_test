10613e442edfbf832af251f9277e094982213b11
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-17 14:46:42 -0300

    [refactor]pagination fixed

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ def get_By_Id(id):
 @app.route('/page/<page>')
 def get_by_page(page):
   page_number = int(page)
-  filtered_Data = list(filter(lambda x: (page_number - 1) * 50 <= x['ID'] < (page_number * 50), data))
+  filtered_Data = list(filter(lambda x: (page_number - 1) * 50 < x['ID'] <= (page_number * 50), data))
   if (not filtered_Data):
     return json.dumps({"response": f"there's no data for the page {page}"})
   return json.dumps(filtered_Data)
