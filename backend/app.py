575702c094a5b5a52e04259d33dcbb1c9ebb99fc
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-17 11:24:09 -0300

    [refactor]data request separated

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,8 @@
 from flask import Flask
-import requests
 import json
+from request import data
 
 app = Flask(__name__)
-
-api_Key = 'tmc12SX5e6ESK7wAcIxCZf7zfTC7Oyz:A6o6BixvhBr32SQ7jxgqFS1JeJqRprJR7wcaU7vDva2nxQzZjb4rddvQAUuTjznGt8'
-headers = {"Authorization": f'ApiKey {api_Key}', "api-version": "v1"}
-res = requests.get('https://workload.us-1.cloudone.trendmicro.com/api/intrusionpreventionrules', headers=headers)
-
-data = res.json()
-data = data['intrusionPreventionRules']
 
 @app.route('/')
 def get_All():
