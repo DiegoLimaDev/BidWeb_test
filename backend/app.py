5b4a97a2b3cbb21e359057549dfad462276c0830
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-17 11:20:15 -0300

    [feat]route for data pagination

--- backend/app.py
+++ backend/app.py
@@ -22,6 +22,14 @@ def get_By_Id(id):
     return json.dumps({"response": f"no object find with the id {id}"})
   return json.dumps(filtered_Data)
 
+@app.route('/page/<page>')
+def get_by_page(page):
+  page_number = int(page)
+  filtered_Data = list(filter(lambda x: (page_number - 1) * 50 <= x['ID'] < (page_number * 50), data))
+  if (not filtered_Data):
+    return json.dumps({"response": f"there's no data for the page {page}"})
+  return json.dumps(filtered_Data)
+
 @app.route('/ndjson')
 def ndjson():
   ndjson_list = []
