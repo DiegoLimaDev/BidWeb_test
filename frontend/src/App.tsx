8d76cefa768c0917f1495d133191cf8e7c908b4a
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-18 11:54:03 -0300

    [feat]layout, globalstyle and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home } from './app/presentation/pages/Home';
+import { Header } from './app/presentation/layout/Header/Header';
+import { GlobalStyle } from './styles/globalStyles';
+import { Footer } from './app/presentation/layout/Footer/Footer';
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
+      <Header />
       <Routes>
-        <Route element={<Home />} path="/" />
+        <Route element={<Home />} path="/:page" />
+        <Route path="/" element={<Navigate to="/1" />} />
       </Routes>
+      <Footer />
     </>
   );
 };
