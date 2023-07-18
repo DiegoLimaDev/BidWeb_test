5580ac4be9daafade0ae5d79851e9230c3a6f316
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-18 11:56:27 -0300

    [feat]HomePage

--- frontend/src/app/presentation/pages/Home.tsx
+++ frontend/src/app/presentation/pages/Home.tsx
@@ -1,3 +1,36 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../application/hooks';
+import { useEffect } from 'react';
+import { fetchIPS } from '../../application/IntrusionPreventionRules/IPS.slice';
+import { Box, Pagination } from '@mui/material';
+import { CardsGridDisplay } from '../components/CardsGridDisplay/CardsGridDisplay';
+
 export const Home = () => {
-  return <div>Home</div>;
+  const { page } = useParams();
+  const dispatch = useAppDispatch();
+  const { ipsData } = useAppSelector((state) => state.ipsSlice);
+  const navigate = useNavigate();
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    navigate(`/${value}`);
+  };
+
+  useEffect(() => {
+    dispatch(fetchIPS(Number(page)));
+  }, [page]);
+
+  return (
+    <div>
+      <CardsGridDisplay ipsData={ipsData} />
+      <Box display={'block'} margin={'2rem auto'} width={'fit-content'}>
+        <Pagination
+          variant="outlined"
+          color="secondary"
+          count={ipsData.length * 2}
+          onChange={handleChange}
+          style={{ height: '4rem', fontSize: '100%' }}
+          size="large"
+        />
+      </Box>
+    </div>
+  );
 };
