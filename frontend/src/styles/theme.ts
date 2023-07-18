12df2fc97656ea913c6239d23b98efa54b748553
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-18 11:53:31 -0300

    [style]theme

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    primaryColor: '#457B9D',
+    secondaryColor: '#E63946',
+    thirdColor: '#A8DADC',
+    fourthColor: '#F1FAEE',
+    fifthColor: '#1D3557',
+    white: '#FBFAF8',
+    black: '#1D1D1D',
+  },
+  sizes: {
+    small: '1rem',
+    small2: '1.2rem',
+    small3: '1.5rem',
+    medium: '2rem',
+    big: '3rem',
+  },
+};
