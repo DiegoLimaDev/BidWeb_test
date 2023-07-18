c90c475212f446e77cc269d76d08f363d66d3aa2
Diego Lima <xxdiegolsxx@gmail.com>
2023-07-18 14:35:12 -0300

    [feat]scrollToTop util added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/application/store.ts';
+import ScrollToTop from './utils/ScrollToTop.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+        <ScrollToTop />
         <App />
       </Provider>
     </BrowserRouter>
